7b846f9950a075f88d9a7138f370b38664f12b02
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-11-22 13:10:41 +0300

    implement 04-http-request-parsing

--- 04-http-request-parsing/src/app.js
+++ 04-http-request-parsing/src/app.js
@@ -1,5 +1,47 @@
+const url = require('url');
+
+const sum = (params) => {
+  const a = parseInt(params.a, 10);
+  const b = parseInt(params.b, 10);
+
+  if (!Number.isNaN(a) && !Number.isNaN(b)) {
+    return { sum: a + b };
+  }
+
+  return null;
+};
+
 const app = (req, res) => {
-  res.end();
+  // handle different request methods
+  if (req.method === 'GET') {
+    // parse request url
+    const { query } = url.parse(req.url, true);
+    const result = sum(query);
+    if (result) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(result, null, 2), 'utf8');
+    } else {
+      res.writeHead(400);
+      res.end();
+    }
+  } else if (req.method === 'POST') {
+    let body = '';
+
+    req.on('data', (data) => {
+      body += data;
+    });
+
+    req.on('end', () => {
+      const result = sum(JSON.parse(body));
+      if (result) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result, null, 2), 'utf8');
+      } else {
+        res.writeHead(400);
+        res.end();
+      }
+    });
+  }
 };
 
 module.exports = app;
