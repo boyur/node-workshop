2ca4f9b00fc7d8ba11aff04c0b5cfb8454eb7b27
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-11-25 17:40:57 +0300

    add more tests on 04-http-request-parsing

--- 04-http-request-parsing/src/app.js
+++ 04-http-request-parsing/src/app.js
@@ -1,49 +1,5 @@
-const url = require('url');
-
-const sum = (params) => {
-  const a = parseInt(params.a, 10);
-  const b = parseInt(params.b, 10);
-
-  if (!Number.isNaN(a) && !Number.isNaN(b)) {
-    return { sum: a + b };
-  }
-
-  return null;
-};
-
 const app = (req, res) => {
-  // handle different request methods
-  if (req.method === 'GET') {
-    // parse request url
-    const { query } = url.parse(req.url, true);
-    const result = sum(query);
-    if (result) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(result, null, 2), 'utf8');
-    } else {
-      res.setHeader('Content-Type', 'text/plain');
-      res.writeHead(400);
-      res.end('Bad Request');
-    }
-  } else if (req.method === 'POST') {
-    let body = '';
-
-    req.on('data', (data) => {
-      body += data;
-    });
-
-    req.on('end', () => {
-      const result = sum(JSON.parse(body));
-      if (result) {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(result, null, 2), 'utf8');
-      } else {
-        res.setHeader('Content-Type', 'text/plain');
-        res.writeHead(400);
-        res.end('Bad Request');
-      }
-    });
-  }
+  res.end('fix me!');
 };
 
 module.exports = app;
